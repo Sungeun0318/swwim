0b1039508b926292f3b1da9c56e05c7d2d7019bc
Sungeun0318 <kimsungeun0318@naver.com>
2025-04-03 15:53:05 +0900

    fix: Firebase 수동 초기화 적용 및 로그인 오류 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'common/navigation/bottom_nav_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-// 필요에 따라 localization 패키지 임포트
+import 'package:swim/login/login_sc.dart';
+import 'package:swim/login/signgup_sc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'login/profile_sc.dart';
+import 'login/search.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBxnDSHtvKVjoPpzeJJ8JfCVF_Ofy4CsPs",
+      appId: "1:54469841565:android:6b8f5caf504693e0a6fa74",
+      messagingSenderId: "54469841565",
+      projectId: "swim-ddaad",
+      storageBucket: "swim-ddaad.firebasestorage.app",
+    ),
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -27,13 +41,20 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Swim Training',
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/success': (context) => const BottomNavScreen(),
+        '/profile': (context) => const ProfileScreen(),
+        '/search': (context) => const PasswordResetScreen(),
+      },
       locale: _locale,
       theme: ThemeData(
-        fontFamily: 'MyCustomFont',  // 전체 텍스트에 커스텀 폰트 적용
+        fontFamily: 'MyCustomFont',
         textTheme: const TextTheme(
           bodyLarge: TextStyle(fontSize: 16),
           bodyMedium: TextStyle(fontSize: 14),
-          // 필요에 따라 다른 스타일 지정
         ),
       ),
       localizationsDelegates: const [
@@ -42,10 +63,10 @@ class _MyAppState extends State<MyApp> {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [
-        Locale('ko', ''), // 한국어
-        Locale('en', ''), // 영어
-        Locale('es', ''), // 스페인어
-        Locale('zh', ''), // 중국어
+        Locale('ko', ''),
+        Locale('en', ''),
+        Locale('es', ''),
+        Locale('zh', ''),
       ],
       home: const BottomNavScreen(),
     );
